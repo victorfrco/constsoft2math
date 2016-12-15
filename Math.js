182b0ecb18d1bdc52a2063a7d703d6d984d5c83d
Robert Willian da Silva <robert.wds88@gmail.com>
2016-12-15 20:08:22 -0300

    Update Math.js
    incluído operações básicas na math.

--- Math.js
+++ Math.js
@@ -1,3 +1,22 @@
-public class Math(){
-
+function math() {}
+math.prototype.operacao = function (a, b, op) {
+	if(op == "+"){
+  	return a+b;
+  }
+  else if(op == "-"){
+  	return a-b;
+  }
+  else if(op == "*"){
+  	return a*b;
+  }
+  else if(op == "/"){
+  	if(b == 0)
+    	return null;
+    else{
+    	return a/b;
+    }
+  }
+  else{
+  	return null;
+  }
 };
