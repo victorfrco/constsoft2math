5d46f3bcae3cf33772c5c0e95b35523105fc269c
OutroLuan <outro.luan@gmail.com>
2016-12-15 20:40:49 -0300

    Primo - Luan
    Metodo que verifica se é primo

--- Math.js
+++ Math.js
@@ -43,3 +43,28 @@ math.prototype.arranjoSimples = function(n1, n2) {
     
     return fatt;
 };
+
+math.prototype.primo = function(num){
+	var primo = 1;
+	if(num === 0){
+    	console.log('Entre com um número diferente de 0');
+		return false;
+	}
+	while(num !== 0){
+		for(var i=2 ; i<num ; i++) {
+			if(num % i === 0) { 
+				primo = 0;
+		     	console.log('Não é primo');
+		     	return false;
+		    }
+		} 
+	    if(num == 2) {
+	    	primo = 0;
+	    	console.log('O número não é primo');
+	    	return false;
+	    }
+	    if(primo === 1 || num === 1)
+			console.log('É primo!');
+			return true;
+	}
+};
